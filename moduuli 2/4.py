b5fc4480cb62e52255369938f7c28b08cb9a25b3
Hussein97-p <alprstigehussein@gmail.com>
2024-08-23 21:48:04 +0300

    edelleen menossa

---
+++ moduuli 2/4.py
@@ -0,0 +1,16 @@
+print("To know the sum, product and average of three Correct numbers .. Follow the Bellow And Press Enter")
+
+x = int(input("First Number: "))
+
+y = int(input("Secound Number: "))
+
+z = int(input("Thierd Number: "))
+
+S = x + y + z 
+print ("The sum of the three numbers is: " , S )
+
+M = x * y * z 
+print("The product of the three numbers is: " , M )
+
+A = S/3 
+print ("The average of the three numbers is : " , round(A ,1) )
---
+++ moduuli 2/6.py
@@ -0,0 +1,10 @@
+import random 
+
+A = list(range(0,9))
+x = random.sample(A , k=3)
+print( x)
+
+
+A1 = list (range(1,6))
+X1= random.sample(A1 , k=4)
+print(X1)
