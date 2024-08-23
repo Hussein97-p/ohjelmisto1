6de76e71cc8458e766cf62ab5a39663fc4f67bd6
Hussein97-p <alprstigehussein@gmail.com>
2024-08-23 23:08:23 +0300

    elleen menossa

---
+++ moduuli 2/5.py
@@ -0,0 +1,12 @@
+
+O = float(input("Enter a Number: "))
+D = float(input("Enter a number: "))
+I = float(input("Enter a number "))
+
+x = O * 20 * 32 * 13.3 
+y = D * 32 * 13.3
+Z = I * 13.3 
+
+G = (x + y + Z) /1000
+
+print(round((G),2), "killo", round((G),5))
