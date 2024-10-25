c107110dbc5fed00f4f3a0e67b4a6285d2d6fecb
Hussein <alprstigehussein@gmail.com>
2024-10-25 13:00:29 +0300

    moduuli 9 / 1 is done

---
+++ moduuli9/1.py
@@ -0,0 +1,4 @@
+from help import Auto
+Auto1 = Auto('ABC-123',142)
+#(rekisteritunnus,huippunopeus,tamanhetkinen_nopeus,kuljettu_matka)#
+print(Auto1.rekisteritunnus,Auto1.huippunopeus,Auto1.tamanhetkinen_nopeus,Auto1.kuljettu_matka)
