d1b29db731a9ad3e28fadcb0d20058845006c919
Hussein <alprstigehussein@gmail.com>
2024-10-25 18:51:10 +0300

    M9 / 1 is doen

--- moduuli9/1.py
+++ moduuli9/1.py
@@ -1,9 +1,8 @@
 class Car:
-    def __init__(self,registration_number, top_speed, current_speed, distance_traveled):
+    def __init__(self,registration_number, top_speed):
         self.registration_number = registration_number
         self.top_speed = top_speed
-        self.current_speed = current_speed
-        self.distance_traveled = distance_traveled
-        
+        self.current_speed = 0
+        self.distance_traveled = 0
 
-my_self = Car("ABC-123",300,50,13)
+my_self = Car("ABC-123",300)
